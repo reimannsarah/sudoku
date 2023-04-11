803270dc2b5028291550acc19ff2a4f5268a6a72
Sarah <reimannsarah45@gmail.com>
2023-04-11 10:32:12 -0700

    good fail test 2
    co-authored-by: Thomas McDowell <tmcdowellddhs@yahoo.com>

--- __tests__/business.test.js
+++ __tests__/business.test.js
@@ -6,4 +6,9 @@ describe('Puzzle', () => {
     const puzzle = new Puzzle();
     expect(puzzle.rows).toEqual({});
   });
+  test('should instantiate a new puzzle', () => {
+    const puzzle = new Puzzle();
+    expect(puzzle.columns).toEqual({});
+  });
+
 });
