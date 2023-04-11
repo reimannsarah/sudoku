6b3ae3e69a01f2fdceba723ce30f1fa6173b35c4
Sarah <reimannsarah45@gmail.com>
2023-04-11 15:16:53 -0700

    good fail for checker test, returns undefined when asked for array.
     co-authored-by: Thomas McDowell <tmcdowellddhs@yahoo.com>
    
    co-authored-by: Raed Alkhanbashi <raed.alkhanbashi@gmail.com>

--- __tests__/business.test.js
+++ __tests__/business.test.js
@@ -1,4 +1,4 @@
-import { Puzzle, Row, Column } from './../src/business.js';
+import { Puzzle, Row, checker} from './../src/business.js';
 
 describe('Puzzle', () => {
 
@@ -6,22 +6,22 @@ describe('Puzzle', () => {
     const puzzle = new Puzzle();
     expect(puzzle.rows).toEqual({});
   });
-  test('should instantiate a new puzzle', () => {
-    const puzzle = new Puzzle();
-    expect(puzzle.columns).toEqual({});
-  });
+  // test('should instantiate a new puzzle', () => {
+  //   const puzzle = new Puzzle();
+  //   expect(puzzle.columns).toEqual({});
+  // });
   test('should add a Row to a Puzzle', () => { 
     const puzzle = new Puzzle();
     const row = new Row(1);
     puzzle.addRow(row);
     expect(puzzle.rows[1]).toEqual({"num1": 1});
   });
-  test('should add a Column to a Puzzle', () => { 
-    const puzzle = new Puzzle();
-    const column = new Column(1);
-    puzzle.addColumn(column);
-    expect(puzzle.columns[1]).toEqual({"num1": 1});
-  });
+  // test('should add a Column to a Puzzle', () => { 
+  //   const puzzle = new Puzzle();
+  //   const column = new Column(1);
+  //   puzzle.addColumn(column);
+  //   expect(puzzle.columns[1]).toEqual({"num1": 1});
+  // });
 
 
 });
@@ -46,22 +46,31 @@ describe('Row', () => {
   });
 });
 
-describe('Column', () => {
+describe('checker', () => {
 
-  test('should return a number', () => {
-    const column = new Column(1);
-    expect(column.num1).toEqual(1); 
-  });
-  test('should return key value pairs', () => {
-    const column = new Column(1,2,3,4,5,6,7,8,9);
-    expect(column.num1).toEqual(1);
-    expect(column.num2).toEqual(2);
-    expect(column.num3).toEqual(3);
-    expect(column.num4).toEqual(4);
-    expect(column.num5).toEqual(5);
-    expect(column.num6).toEqual(6);
-    expect(column.num7).toEqual(7);
-    expect(column.num8).toEqual(8);
-    expect(column.num9).toEqual(9);
+  test('should return an array from the key value pairs', () => {
+    const row = new Row(1,2,3,4,5,6,7,8,9);
+    const rowArray = checker(row);
+    expect(rowArray).toEqual([1,2,3,4,5,6,7,8,9]);
   });
 });
+
+
+// describe('Column', () => {
+
+//   test('should return a number', () => {
+//     const column = new Column(1);
+//     expect(column.num1).toEqual(1); 
+//   });
+//   test('should return key value pairs', () => {
+//     const column = new Column(1,2,3,4,5,6,7,8,9);
+//     expect(column.num1).toEqual(1);
+//     expect(column.num2).toEqual(2);
+//     expect(column.num3).toEqual(3);
+//     expect(column.num4).toEqual(4);
+//     expect(column.num5).toEqual(5);
+//     expect(column.num6).toEqual(6);
+//     expect(column.num7).toEqual(7);
+//     expect(column.num8).toEqual(8);
+//     expect(column.num9).toEqual(9);
+//   });
