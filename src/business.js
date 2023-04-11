a4be509597bfbe53a01411a5dd8b99c4f5dc2b63
Sarah <reimannsarah45@gmail.com>
2023-04-11 15:20:37 -0700

    pass test for checker function to get object values of row object.
    co-authored-by: Thomas McDowell <tmcdowellddhs@yahoo.com>
    co-authored-by: Raed Alkhanbashi <raed.alkhanbashi@gmail.com>

--- src/business.js
+++ src/business.js
@@ -4,18 +4,38 @@ export function Puzzle() {
 }
 
 Puzzle.prototype.addRow = function(row) {
-  this.rows[row.number] = row;
+  this.rows[row.num1] = row;
 };
 
-Puzzle.prototype.addColumn = function(column) {
-  this.columns[column.number] = column;
-};
-
-export function Row(number) {
-  this.number = number;
+export function Row(num1,num2,num3,num4,num5,num6,num7,num8,num9) {
+  this.num1 = num1;
+  this.num2 = num2;
+  this.num3 = num3;
+  this.num4 = num4;
+  this.num5 = num5;
+  this.num6 = num6;
+  this.num7 = num7;
+  this.num8 = num8;
+  this.num9 = num9;
 }
 
-export function Column(number) {
-  this.number = number;
+export function checker(row) {
+  const rowArray = Object.values(row);
+  return rowArray;
 }
+// Puzzle.prototype.addColumn = function(column) {
+//   this.columns[column.num1] = column;
+// };
+
+// export function Column(num1,num2,num3,num4,num5,num6,num7,num8,num9) {
+//   this.num1 = num1;
+//   this.num2 = num2;
+//   this.num3 = num3;
+//   this.num4 = num4;
+//   this.num5 = num5;
+//   this.num6 = num6;
+//   this.num7 = num7;
+//   this.num8 = num8;
+//   this.num9 = num9;
+// }
 
